20b72862c48bab92dd7266926797f34749f081d2
Shubham Undirwade <89636942+shubhamundirwade@users.noreply.github.com>
2022-05-10 10:14:51 +0530

    Add files via upload

---
+++ qrco.py
@@ -0,0 +1,5 @@
+
+import qrcode
+
+img = qrcode.make("https://www.linkedin.com/in/shubham-undirwade/")
+img.save("LinkedIn.jpg")
---
+++ qrcode.py
@@ -0,0 +1,4 @@
+import qrcode
+
+img = qrcode.make("India is a country with many religions. I love India.")
+img.save("youtubeQR.jpg")
